5ca396ad3a1e1dfa13535e0f3d64bc1c3073fc3c
JacksonFA <jackson.f205@gmail.com>
2024-04-04 16:03:02 -0300

    new(devops): adds DigitalO Kube to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,15 @@ resource "digitalocean_database_db" "db-name" {
     digitalocean_database_cluster.db-cluster
   ]
 }
+
+resource "digitalocean_kubernetes_cluster" "passin" {
+  name    = "passin"
+  region  = "nyc1"
+  version = "1.29.1-do.0"
+
+  node_pool {
+    name       = "node-pool"
+    size       = "s-1vcpu-2gb"
+    node_count = 3
+  }
+}
